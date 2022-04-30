e41e9456c69f041b345ab05da0a83cd76198b303
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-04-30 17:02:09 +0500

    Initial Commit

---
+++ VoiceOver Designer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VoiceOver Designer
+//
+//  Created by Mikhail Rubanov on 30.04.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
