9897d07d96982b99693ac2a576af1bcf37787f33
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-05-05 22:53:26 +0500

    Add drag’n’drop to open file

--- VoiceOver Designer/ScreenReceiverViewController.swift
+++ VoiceOver Designer/ScreenReceiverViewController.swift
@@ -28,6 +28,66 @@ class ScreenReceiverViewController: NSViewController {
         
         view.window?.contentViewController = controller
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view().delegate = self
+    }
+    
+    func view() -> ScreenReceiverView {
+        view as! ScreenReceiverView
+    }
+}
+
+extension ScreenReceiverViewController: DragNDropDelegate {
+    func didDrag(image: NSImage) {
+        show(with: image)
+    }
+}
+
+protocol DragNDropDelegate: AnyObject {
+    func didDrag(image: NSImage)
+}
+
+class ScreenReceiverView: NSView {
+    
+    weak var delegate: DragNDropDelegate?
+    
+    @IBOutlet weak var dragHereView: NSImageView!
+    @IBOutlet weak var dragHereLabel: NSTextField!
+    
+    private var isWaitingForFile: Bool = false {
+        didSet {
+            dragHereView.isHidden = !isWaitingForFile
+            dragHereLabel.isHidden = isWaitingForFile
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // TODO: Add another files
+        registerForDraggedTypes([.png, .fileURL, .fileContents])
+    }
+    
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        isWaitingForFile = true
+        return .copy
+    }
+    
+    override func draggingEnded(_ sender: NSDraggingInfo) {
+        isWaitingForFile = false
+    }
+    
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        let pasteboard = sender.draggingPasteboard
+        
+        guard let image = NSImage(pasteboard: pasteboard) else { return false }
+        
+        delegate?.didDrag(image: image)
+        
+        return true
+    }
 }
 
 extension VODesignDocument {
