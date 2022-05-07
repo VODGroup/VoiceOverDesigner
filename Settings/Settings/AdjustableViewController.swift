deded3286e084ef64e558ff77d6870611fa63959
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-05-07 20:22:31 +0500

    Add classes to adjustable

---
+++ Settings/Settings/AdjustableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AdjustableViewController.swift
+//  Settings
+//
+//  Created by Mikhail Rubanov on 07.05.2022.
+//
+
+import AppKit
+
+class AdjustableViewController: NSViewController {
+    
+}
+
+class AdjustableView: NSView {
+    
+}
+
