77da4135de1913bc3d18987a9f709acf5c310a37
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-07-30 19:48:47 +0500

    Open document browser as root

--- VoiceOver Preview/VoiceOver Preview/SceneDelegate.swift
+++ VoiceOver Preview/VoiceOver Preview/SceneDelegate.swift
@@ -11,12 +11,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        documentBrowser.delegate = self
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = documentBrowser
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +52,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
+    lazy var documentBrowser: UIDocumentBrowserViewController = {
+        let controller = UIDocumentBrowserViewController()
+        controller.allowsPickingMultipleItems = false
+        controller.allowsDocumentCreation = false
+        return controller
+    }()
 }
 
+import Document
+extension SceneDelegate: UIDocumentBrowserViewControllerDelegate {
+    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
+        guard let url = documentURLs.first else {
+            return
+        }
+        
+        let document = VODesignDocument(fileURL: url)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let preview = storyboard.instantiateInitialViewController() as! PreviewViewController
+        preview.open(document: document)
+            
+        window?.rootViewController = preview
+    }
+}
