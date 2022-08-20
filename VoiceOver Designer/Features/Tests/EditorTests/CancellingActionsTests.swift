44f8beeef84e4b435499a335dfa35ee225422e68
drucelweisse <drucelweisse@gmail.com>
2022-08-20 22:44:37 +0300

    Add other cancel actions tests

--- VoiceOver Designer/Features/Tests/EditorTests/CancellingActionsTests.swift
+++ VoiceOver Designer/Features/Tests/EditorTests/CancellingActionsTests.swift
@@ -33,4 +33,58 @@ class CancellingActionsTests: EditorAfterDidLoadTests {
         XCTAssertEqual(sut.document.controls[0].frame, rect10to50)
     
     }
+    
+    func test_CancelTranslateActionShouldResetFrame() async throws {
+        
+        
+        await MainActor.run {
+            drawRect_10_60()
+        }
+        
+        // Copy
+        
+        await MainActor.run {
+            sut.mouseDown(on: .coord(15))
+            controller.controlsView.escListener.delegate?.didPressed()
+            sut.mouseUp(on: .coord(50))
+        }
+
+        
+        XCTAssertEqual(sut.document.controls.count, 1)
+        XCTAssertEqual(sut.document.controls[0].frame, rect10to50)
+    
+    }
+    
+    
+    
+    func test_CancelNewControlActionShouldDeleteNewControl() async throws {
+        
+        await MainActor.run {
+            sut.mouseDown(on: start10)
+            controller.controlsView.escListener.delegate?.didPressed()
+            sut.mouseUp(on: start10.offset(x: 10, y: 50))
+        }
+        
+        XCTAssertEqual(sut.document.controls.count, 0)
+        XCTAssertTrue(sut.document.controls.isEmpty)
+    
+    }
+    
+    
+    func test_CancelResizeActionShouldResetFrameSize() async throws {
+        drawRect(from: start10, to: end60)
+        XCTAssertEqual(drawnControls.count, 1)
+        
+        await MainActor.run {
+            sut.mouseDown(on: .coord(60-1)) // Not inclued border
+            controller.controlsView.escListener.delegate?.didPressed()
+            sut.mouseDragged(on: .coord(20))
+        }
+        
+
+        
+        XCTAssertEqual(drawnControls.count, 1)
+        XCTAssertEqual(drawnControls[0].frame, rect10to50)
+    }
+    
 }
