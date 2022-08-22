0c27284a54b0600bb7fa06da9972998463dc6f50
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-08-23 00:09:46 +0500

    Add disabling part of adjustable testing

--- VoiceOverDesignerUITests/AdjustableTest.swift
+++ VoiceOverDesignerUITests/AdjustableTest.swift
@@ -4,17 +4,26 @@ class AdjustableTest: DocumentTests {
     func test_whenCreateAdjustable_andSelectAnotherVariant_shouldUpdateTitle() {
         canvas.drawRectInCenter()
         
-        settings
-            .inputLabel("Pizza ize")
-            .inputValue("Small")
-            .clickAdjustable()
+        XCTContext.runActivity(named: "Fill adjustable") { _ in
+            settings
+                .inputLabel("Pizza ize")
+                .inputValue("Small")
+                .clickAdjustable()
+            
+            project.verify(controlDescription: "Pizza ize: Small. Adjustable.")
+            
+            settings
+                .addAdjustableVariant("Meium")
+                .selectAdjustable("Meium")
+            
+            project.verify(controlDescription: "Pizza ize: Meium, 2 of 2. Adjustable.")
+        }
         
-        project.verify(controlDescription: "Pizza ize: Small. Adjustable.")
-        
-        settings
-            .addAdjustableVariant("Meium")
-            .selectAdjustable("Meium")
-        
-        project.verify(controlDescription: "Pizza ize: Meium, 2 of 2. Adjustable.")
+        XCTContext.runActivity(named: "Disable adjustable") { _ in
+            settings
+                .clickAdjustable()
+            
+            project.verify(controlDescription: "Pizza ize: Meium")
+        }
     }
 }
