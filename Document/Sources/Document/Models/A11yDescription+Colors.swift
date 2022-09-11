f6413740afcd63dd482d7472e7f1da9719a43a77
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-09-11 18:52:56 +0500

    Extract color managment

--- Document/Sources/Document/Models/A11yDescription+Colors.swift
+++ Document/Sources/Document/Models/A11yDescription+Colors.swift
@@ -8,30 +8,7 @@ extension A11yDescription {
     static let colorAlpha: CGFloat = A11yControl.Config().normalAlpha
     
     public var color: Color {
-        guard isAccessibilityElement else {
-            return Self.ignoreColor.withAlphaComponent(Self.colorAlpha)
-        }
-        
-        guard isAdjustable else {
-            return Self.adjustableColor.withAlphaComponent(Self.colorAlpha)
-        }
-        
-        return (isValid ? Self.validColor: Self.invalidColor).withAlphaComponent(Self.colorAlpha)
-    }
-    
-    static var invalidColor: Color {
-        Color.systemOrange
-    }
-    
-    static var validColor: Color {
-        Color.systemGreen
-    }
-    
-    static var ignoreColor: Color {
-        Color.systemGray
-    }
-    
-    static var adjustableColor: Color {
-        Color.systemPurple
+        .colorFor(element: self)
+        .withAlphaComponent(Self.colorAlpha)
     }
 }
