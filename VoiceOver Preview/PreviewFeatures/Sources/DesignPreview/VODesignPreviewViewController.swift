6a433420e0c8f9e49068488f3cb34c6e0a859496
Rubanov Mikhail <m.rubanov@dodopizza.com>
2022-11-19 16:03:55 +0500

    Add gestures to draw elements

--- VoiceOver Preview/PreviewFeatures/Sources/DesignPreview/VODesignPreviewViewController.swift
+++ VoiceOver Preview/PreviewFeatures/Sources/DesignPreview/VODesignPreviewViewController.swift
@@ -15,10 +15,10 @@ public final class VODesignPreviewViewController: UIViewController {
         let storyboard = UIStoryboard(name: "VODesignPreviewViewController",
                                       bundle: .module)
         let preview = storyboard.instantiateInitialViewController() as! VODesignPreviewViewController
-        preview.open(document: document)
+        preview.open(document: document) // TODO: load document firstly, then load controller
         return preview
     }
-
+    
     private var presenter: CanvasPresenter!
     
     private var document: VODesignDocument!
@@ -26,36 +26,22 @@ public final class VODesignPreviewViewController: UIViewController {
         self.document = document
         self.presenter = CanvasPresenter(document: document)
     }
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         loadAndDraw()
-        
-        NotificationCenter.default.addObserver(
-            self, selector: #selector(Self.documentStateChanged(_:)),
-            name: UIDocument.stateChangedNotification, object: document)
-    }
-    
-    public override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    func setImage() {
-        view().image = presenter.document.image
-    }
-    }
-    
-    @objc
-    func documentStateChanged(_ notification: Notification) {
-        document.printState()
-        
-        if document.documentState == .progressAvailable {
-            loadAndDraw()
-        }
+        addDocumentStateObserving()
+
     }
     
     func view() -> VODesignPreviewView {
         view as! VODesignPreviewView
     }
+}
+
+// MARK: - Loading
+extension VODesignPreviewViewController {
     
     private func loadAndDraw() {
         self.view().canvas.removeAll()
@@ -75,6 +61,78 @@ public final class VODesignPreviewViewController: UIViewController {
             ui: view().canvas,
             screenUI: view())
         
-        setImage()
+        view().image = presenter.document.image
+        
+        addGestures() // TODO: If document would drawn several times gestures will be added a lot
+    }
+}
+
+// MARK: - Gesture
+extension VODesignPreviewViewController {
+    func addGestures() {
+        let drawingGesture = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(didPan(gesture:)))
+        view().canvas.addGestureRecognizer(drawingGesture)
+        
+        let selectionGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(didTap(gesture:)))
+        view().canvas.addGestureRecognizer(selectionGesture)
+    }
+    
+    @objc func didTap(gesture: UITapGestureRecognizer) {
+        let location = gesture.location(in: gesture.view)
+        
+        switch gesture.state {
+        case .began:
+            break
+        case .changed:
+            break
+        case .ended:
+            presenter.mouseDown(on: location)
+            presenter.mouseUp(on: location)
+        case .cancelled: break
+        case .failed: break
+        case .possible: break
+        @unknown default:
+            break
+        }
+    }
+    
+    @objc func didPan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: gesture.view)
+        
+        switch gesture.state {
+        case .began:
+            presenter.mouseDown(on: location)
+        case .changed:
+            presenter.mouseDragged(on: location)
+        case .ended:
+            presenter.mouseUp(on: location)
+        case .cancelled: break
+        case .failed: break
+        case .possible: break
+        @unknown default:
+            break
+        }
+    }
+}
+
+// MARK: - iCloud sync
+extension VODesignPreviewViewController {
+    private func addDocumentStateObserving() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(Self.documentStateChanged(_:)),
+            name: UIDocument.stateChangedNotification, object: document)
+    }
+    
+    @objc
+    func documentStateChanged(_ notification: Notification) {
+        document.printState()
+        
+        if document.documentState == .progressAvailable {
+            loadAndDraw()
+        }
     }
 }
