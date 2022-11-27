6ff1ae74c50ba1f94428eb5432f0dd402ed390f3
Rubanov Mikhail <m.rubanov@dodopizza.com>
2022-11-27 15:21:27 +0500

    Add additional tests for grouping

--- Shared/Sources/Document/Models/AccessibilityView/AccessibilityView+Grouping.swift
+++ Shared/Sources/Document/Models/AccessibilityView/AccessibilityView+Grouping.swift
@@ -2,10 +2,32 @@ import Foundation
 
 extension Array where Element == any AccessibilityView {
     public mutating func wrapInContainer(indexes: IndexSet) {
-        let element = remove(at: indexes.first!) as! A11yDescription
+        guard indexes.count > 0 else { return }
         
-        append(A11yContainer(elements: [element],
-                             frame: element.frame,
-                             label: "Test"))
+        var extractedElements = [A11yDescription]()
+        
+        for index in indexes.reversed() {
+            let element = remove(at: index) as! A11yDescription
+            extractedElements.append(element)
+        }
+        
+        let container = A11yContainer(
+            elements: extractedElements,
+            frame: extractedElements.map(\.frame).commonFrame,
+            label: "Test")
+        
+        insert(container, at: indexes.first!)
+    }
+}
+
+extension Array where Element == CGRect {
+    var commonFrame: CGRect {
+        var result = first!
+        
+        for rect in self.dropFirst() {
+            result = result.union(rect)
+        }
+        
+        return result
     }
 }
--- Shared/Tests/DocumentTests/AccessibilityViewArrayGroupingTests.swift
+++ Shared/Tests/DocumentTests/AccessibilityViewArrayGroupingTests.swift
@@ -13,5 +13,42 @@ final class AccessibilityViewArrayGroupingTests: XCTestCase {
         XCTAssertTrue(array.first is A11yContainer)
     }
     
-
+    func test_wrapTwoElementsInContainer() {
+        var array = [any AccessibilityView]()
+        array.append(A11yDescription.testMake())
+        array.append(A11yDescription.testMake())
+        
+        array.wrapInContainer(indexes: [0, 1])
+        
+        XCTAssertEqual(array.count, 1)
+        XCTAssertTrue(array.first is A11yContainer)
+    }
+    
+    func test_wrapTwoElementsOfThree_shouldPlaceContainerToFirstPosition() {
+        var array = [any AccessibilityView]()
+        array.append(A11yDescription.testMake())
+        array.append(A11yDescription.testMake())
+        array.append(A11yDescription.testMake())
+        
+        array.wrapInContainer(indexes: [0, 1])
+        
+        XCTAssertEqual(array.count, 2)
+        XCTAssertTrue(array.first is A11yContainer)
+    }
+    
+    func test_wrapTwoElements_shouldUnionFrames() {
+        var array = [any AccessibilityView]()
+        array.append(A11yDescription.testMake(
+            frame: CGRect(x: 10, y: 10, width: 10, height: 10)))
+        array.append(A11yDescription.testMake(
+            frame: CGRect(x: 20, y: 20, width: 10, height: 10)))
+        
+        array.wrapInContainer(indexes: [0, 1])
+        
+        XCTAssertTrue(array.first is A11yContainer)
+        XCTAssertEqual(array.first?.frame,
+                      CGRect(x: 10, y: 10, width: 20, height: 20))
+    }
+    
+    // TODO: Index inside container
 }
