ae4c945dbc7bbec3fdb438c5fd8bcd557f765754
Rubanov Mikhail <m.rubanov@dodopizza.com>
2022-12-10 16:07:54 +0500

    Fix tests

--- VoiceOver Designer/Features/Tests/CanvasAppKitTests/Doubles/FakeEditorUI.swift
+++
@@ -1,9 +0,0 @@
-@testable import Canvas
-import CoreGraphics
-
-class FakeCanvasUI: CanvasPresenterUIProtocol {
-    var image: CGImage?
-    func image(at frame: CGRect) async -> CGImage? {
-        return image
-    }
-}
