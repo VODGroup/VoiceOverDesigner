328e82a47d07ddfd04e16268c15d2f32b8beae31
Mikhail Rubanov <m.rubanov@dodopizza.com>
2023-01-05 23:43:38 +0500

    Check iCloud folder only for release builds

--- Shared/Sources/Document/Documents/iCloudContainer.swift
+++ Shared/Sources/Document/Documents/iCloudContainer.swift
@@ -12,12 +12,12 @@ public let containerId = "iCloud.com.akaDuality.VoiceOver-Designer"
 public let iCloudContainer: URL = {
     let fileManager = FileManager.default
     let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-    let iCloudDirectory = fileManager.url(forUbiquityContainerIdentifier: containerId)
     
     let rootURL: URL
 #if DEBUG
     rootURL = documentDirectory
 #else
+    let iCloudDirectory = fileManager.url(forUbiquityContainerIdentifier: containerId)
     rootURL = iCloudDirectory ?? documentDirectory // When iCloud is disabled
 #endif
     
