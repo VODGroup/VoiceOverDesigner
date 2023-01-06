fe8cdb817f8ffc1854b9547e4fcb097fd12f3204
Mikhail Rubanov <m.rubanov@dodopizza.com>
2023-01-07 00:01:12 +0500

    Fix iCloud folder visibility

--- Shared/Sources/Document/Documents/iCloudContainer.swift
+++ Shared/Sources/Document/Documents/iCloudContainer.swift
@@ -9,17 +9,25 @@ import Foundation
 
 public let containerId = "iCloud.com.akaDuality.VoiceOver-Designer"
 
+private let fileManager = FileManager.default
+
 public let iCloudContainer: URL = {
-    let fileManager = FileManager.default
-    let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-    
-    let rootURL: URL
 #if DEBUG
-    rootURL = documentDirectory
+    print("Use documents directory in DEBUG")
+    return documentDirectory
 #else
-    let iCloudDirectory = fileManager.url(forUbiquityContainerIdentifier: containerId)
-    rootURL = iCloudDirectory ?? documentDirectory // When iCloud is disabled
+    print("Use \(iCloudDirectory != nil ? "cloud" : "documents") directory in RELEASE")
+    return iCloudDirectory ?? documentDirectory // When iCloud is disabled
 #endif
-    
-    return rootURL
 }()
+
+private var iCloudDirectory: URL? {
+    fileManager
+        .url(forUbiquityContainerIdentifier: containerId)?
+        .appending(path: "Documents")
+}
+
+private var documentDirectory: URL {
+    fileManager
+        .urls(for: .documentDirectory, in: .userDomainMask).first!
+}
