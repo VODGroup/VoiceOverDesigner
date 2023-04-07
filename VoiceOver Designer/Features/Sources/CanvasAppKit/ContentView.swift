cce74c9511caaf74555371be61d9a8b272e92cb7
Mikhail Rubanov <akaDuality@yandex.ru>
2023-04-07 18:42:26 -0300

    Fix text recognition from image

--- VoiceOver Designer/Features/Sources/CanvasAppKit/ContentView.swift
+++ VoiceOver Designer/Features/Sources/CanvasAppKit/ContentView.swift
@@ -24,4 +24,34 @@ class ContentView: FlippedView {
     var image: NSImage? {
         imageViews.first?.image
     }
+    
+    func image(at frame: CGRect) -> CGImage? {
+        let imageView = imageViews.first { imageView in
+            imageView.frame.intersects(frame)
+        }
+        
+        guard let image = imageView?.image,
+              let imageViewFrame = imageView?.frame
+        else { return nil }
+        
+        var frameInImageCoordinates = frame
+            .scaled(image.recommendedLayerContentsScale(1))
+            .origin(to: imageViewFrame)
+        
+        let cgImage = image
+            .cgImage(forProposedRect: &frameInImageCoordinates,
+                     context: nil,
+                     hints: nil)?
+            .cropping(to: frameInImageCoordinates)
+        
+        return cgImage
+    }
+}
+
+extension CGRect {
+    func origin(to parentFrame: CGRect) -> CGRect {
+        CGRect(origin: CGPoint(x: origin.x - parentFrame.origin.x,
+                               y: origin.y - parentFrame.origin.y),
+               size: size)
+    }
 }
