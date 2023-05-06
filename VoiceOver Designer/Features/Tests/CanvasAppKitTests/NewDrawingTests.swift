5162facbcb319706443ea28b8e833735049fea2b
drucelweisse <drucelweisse@gmail.com>
2023-05-06 23:03:09 +0600

    edit test to expect non-nil value

--- VoiceOver Designer/Features/Tests/CanvasAppKitTests/NewDrawingTests.swift
+++ VoiceOver Designer/Features/Tests/CanvasAppKitTests/NewDrawingTests.swift
@@ -79,6 +79,14 @@ class NewDrawingTests: CanvasAfterDidLoadTests {
         
         XCTAssertEqual(document.artboard.controlsWithoutFrames.count, 0)
     }
+        
+    func test_createControlsWhenDocumentImageNil() {
+        // TODO: Remove image?
+        
+        sut.mouseDown(on: start10)
+        sut.mouseUp(on: end60)
+        XCTAssertNotNil(drawnControls.first, "Should create control")
+    }
     
     func addFrame() {
         let image = try! XCTUnwrap(Sample().image(name: Sample.image3xScale))
