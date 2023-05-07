2fa9e8fa96da5b476fc0bc5e485758b59682bd8b
Mikhail Rubanov <akaDuality@yandex.ru>
2023-05-07 19:30:38 -0300

    Can fit scale according to width and height
    Fix position

--- VoiceOver Designer/Features/Sources/CanvasAppKit/CanvasView.swift
+++ VoiceOver Designer/Features/Sources/CanvasAppKit/CanvasView.swift
@@ -109,7 +109,7 @@ class CanvasView: FlippedView {
         
         // TODO: Check width
         let scrollViewVisibleHeight = scrollView.frame.height
-        return scrollViewVisibleHeight / contentSize.height
+        return min(scrollViewVisibleHeight / contentSize.height, scrollView.frame.width / contentSize.width)
     }
     
     // MARK: - Image
--- VoiceOver Designer/Features/Sources/CanvasAppKit/CenteredClipView.swift
+++ VoiceOver Designer/Features/Sources/CanvasAppKit/CenteredClipView.swift
@@ -6,13 +6,14 @@ class CenteredClipView: NSClipView
         var rect = super.constrainBoundsRect(proposedBounds)
         
         if let containerView = documentView {
+            let containerSize = containerView.intrinsicContentSize // can change on current layout pass and have not yet affect just containerView.frame
             
-            if (rect.size.width > containerView.frame.size.width) {
-                rect.origin.x = (containerView.frame.width - rect.width) / 2
+            if (rect.size.width > containerSize.width) {
+                rect.origin.x = (containerSize.width - rect.width) / 2
             }
             
-            if(rect.size.height > containerView.frame.size.height) {
-                rect.origin.y = (containerView.frame.height - rect.height) / 2
+            if(rect.size.height > containerSize.height) {
+                rect.origin.y = (containerSize.height - rect.height) / 2
             }
         }
         
