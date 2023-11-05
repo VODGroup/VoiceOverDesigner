54f769304fcfc76a23d9b92a2baabf4d46c37738
Mikhail Rubanov <akaDuality@yandex.ru>
2023-11-05 20:02:09 -0300

    Bump minimal macOS support to v13 to use `path()` instead of `path`
    Path() returns relative part that is great for image loading stored in relative folder

--- VoiceOver Designer/Features/Package.swift
+++ VoiceOver Designer/Features/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Features",
     defaultLocalization: "en",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "CanvasAppKit",
