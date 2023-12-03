c919901b7b4081fe70d129f26813ee821893bbad
Mikhail Rubanov <akaDuality@yandex.ru>
2023-12-02 22:01:58 -0300

    Remove sample from tests

--- Shared/Tests/DocumentTests/Models/Order/DocumentPresenterTests+Movement.swift
+++ Shared/Tests/DocumentTests/Models/Order/DocumentPresenterTests+Movement.swift
@@ -17,46 +17,51 @@ class DocumentPresenterTests_Movement: XCTestCase {
     var frame: Frame!
     var title: A11yDescription!
     var settingsButton: A11yDescription!
-    var tabsContainer: A11yContainer!
     
     var sut: DocumentPresenter!
     var undoManager: UndoManager?
-//    let testDocumentName = "Test"
+    let testDocumentName = "Test"
     
-    // TODO: use fake document and artboard.assert(labels: ...)
     override func setUpWithError() throws {
         try super.setUpWithError()
         
-        let document: VODesignDocument = try Sample().document(name: FileSample.artboard, testCase: self)
-//        let document = VODesignDocument(fileName: testDocumentName)
+        let document = VODesignDocument.testDocument(name: testDocumentName, testCase: self)
         
         undoManager = document.undoManager
         sut = DocumentPresenter(document: document)
         
         artboard = document.artboard
-//        sut.add(image: Sample().image3x(), origin: .zero)
+        
+        undoManager?.disableUndoRegistration()
+        sut.add(image: Sample().image3x(), origin: .zero)
         
         frame = try XCTUnwrap(artboard.frames.first)
-        title = try XCTUnwrap(frame.elements[0] as? A11yDescription)
-        settingsButton = try XCTUnwrap(frame.elements[1] as? A11yDescription)
-        tabsContainer = try XCTUnwrap(frame.elements.last as? A11yContainer)
+        frame.label = "Frame"
+        
+        title = A11yDescription.testMake(label: "Title")
+        settingsButton = A11yDescription.testMake(label: "Settings")
+        
+        sut.append(control: title)
+        sut.append(control: settingsButton)
+        sut.append(control: A11yDescription.testMake(label: "Coins"))
+        sut.append(control: A11yDescription.testMake(label: "Gift"))
+        undoManager?.enableUndoRegistration()
     }
     
     override func tearDown() {
         title = nil
         settingsButton = nil
-        tabsContainer = nil
         
-//        try? VODesignDocument.removeTestDocument(name: testDocumentName)
+        try? VODesignDocument.removeTestDocument(name: testDocumentName)
         
         // Document will be removed at tearDown by Sampler
         super.tearDown()
     }
     
     func testDefaultState() {
-        XCTAssertEqual(frame.elements.count, 10)
-        XCTAssertEqual(artboard.elements.count, 2)
-        XCTAssertEqual(labels[0...2], ["Привет, Михаил", "Настройки", "186 додокоинов"])
+        XCTAssertEqual(frame.elements.count, 4)
+        XCTAssertEqual(artboard.elements.count, 1)
+        XCTAssertEqual(labels[0...2], ["Title", "Settings", "Coins"])
     }
     
     func test_2elementsInFrame_whenDropOnElement_shouldCreateContainer() throws {
@@ -67,15 +72,15 @@ class DocumentPresenterTests_Movement: XCTestCase {
         
         XCTAssertTrue(result)
         
-        XCTAssertEqual(labels[0...2], ["Container", "186 додокоинов", "История заказов"])
+        XCTAssertEqual(labels[0...1], ["Container", "Coins"])
         
         undo()
-        XCTAssertEqual(labels[0...2], ["Привет, Михаил", "Настройки", "186 додокоинов"])
+        XCTAssertEqual(labels[0...2], ["Title", "Settings", "Coins"])
         
         redo()
         let newContainer2 = try XCTUnwrap(frame.elements.first as? A11yContainer, "wrap in container")
         XCTAssertEqual(newContainer2.elements.count, 2)
-        XCTAssertEqual(labels[0...2], ["Container", "Привет, Михаил", "Настройки"])
+        XCTAssertEqual(labels[0...1], ["Container", "Coins"])
     }
 
     func test_2elementsInFrame_whenDropElementAfter2ndElement_shouldRearrange() {
@@ -86,34 +91,34 @@ class DocumentPresenterTests_Movement: XCTestCase {
         
         XCTAssertTrue(result)
         
-        XCTAssertEqual(labels[0...1], ["Настройки", "Привет, Михаил"])
+        XCTAssertEqual(labels[0...1], ["Settings", "Title"])
         
         undo()
-        XCTAssertEqual(labels[0...1], ["Привет, Михаил", "Настройки"])
+        XCTAssertEqual(labels[0...1], ["Title", "Settings"])
         
         redo()
-        XCTAssertEqual(labels[0...1], ["Настройки", "Привет, Михаил"])
+        XCTAssertEqual(labels[0...1], ["Settings", "Title"])
     }
     
     func test_moveElementAfterFrame_shouldMoveToArtboardLevel() throws {
         let result = sut.drag(
             title,
             over: nil,
-            insertAtIndex: 2)
+            insertAtIndex: 1) // After frame
         
         XCTAssertTrue(result)
-        XCTAssertEqual(artboard.elements.count, 3, "Add element on artboard's level")
-        XCTAssertEqual(artboard.elements.last?.label, "Привет, Михаил")
-        XCTAssertEqual(labels[0...1], ["Настройки", "186 додокоинов"])
+        XCTAssertEqual(artboard.elements.count, 2, "Add element on artboard's level")
+        XCTAssertEqual(artboard.elements.last?.label, "Title")
+        XCTAssertEqual(labels[0...1], ["Settings", "Coins"])
         
         undo()
-        XCTAssertEqual(artboard.elements.count, 2)
+        XCTAssertEqual(artboard.elements.count, 1, "Only frame")
         XCTAssertEqual(artboard.elements.last?.label, "Frame")
-        XCTAssertEqual(labels[0...2], ["Привет, Михаил", "Настройки", "186 додокоинов"])
+        XCTAssertEqual(labels[0...2], ["Title", "Settings", "Coins"])
         
         redo()
-        XCTAssertEqual(artboard.elements.last?.label, "Привет, Михаил")
-        XCTAssertEqual(labels[0...1], ["Настройки", "186 додокоинов"])
+        XCTAssertEqual(artboard.elements.last?.label, "Title")
+        XCTAssertEqual(labels[0...1], ["Settings", "Coins"])
     }
     
     func test_moveElementOutOfFrame_shouldMoveToArtboardLevel() throws {
@@ -123,18 +128,18 @@ class DocumentPresenterTests_Movement: XCTestCase {
             insertAtIndex: -1)
         
         XCTAssertTrue(result)
-        XCTAssertEqual(artboard.elements.count, 3, "Add element on artboard's level")
-        XCTAssertEqual(artboard.elements.last?.label, "Привет, Михаил")
-        XCTAssertEqual(labels[0...1], ["Настройки", "186 додокоинов"])
+        XCTAssertEqual(artboard.elements.count, 2, "Add element on artboard's level")
+        XCTAssertEqual(artboard.elements.last?.label, "Title")
+        XCTAssertEqual(labels[0...1], ["Settings", "Coins"])
         
         undo()
-        XCTAssertEqual(artboard.elements.count, 2)
+        XCTAssertEqual(artboard.elements.count, 1)
         XCTAssertEqual(artboard.elements.last?.label, "Frame")
-        XCTAssertEqual(labels[0...2], ["Привет, Михаил", "Настройки", "186 додокоинов"])
+        XCTAssertEqual(labels[0...2], ["Title", "Settings", "Coins"])
         
         redo()
-        XCTAssertEqual(artboard.elements.last?.label, "Привет, Михаил")
-        XCTAssertEqual(labels[0...1], ["Настройки", "186 додокоинов"])
+        XCTAssertEqual(artboard.elements.last?.label, "Title")
+        XCTAssertEqual(labels[0...1], ["Settings", "Coins"])
     }
     
     
