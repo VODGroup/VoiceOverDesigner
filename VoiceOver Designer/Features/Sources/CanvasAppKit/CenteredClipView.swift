8e50d596574f8a4559cc456fdbef42592eee5c1c
Mikhail Rubanov <akaDuality@yandex.ru>
2023-12-26 22:36:37 -0300

    Restore centering

--- VoiceOver Designer/Features/Sources/CanvasAppKit/CenteredClipView.swift
+++ VoiceOver Designer/Features/Sources/CanvasAppKit/CenteredClipView.swift
@@ -2,24 +2,25 @@ import AppKit
 
 class CenteredClipView: NSClipView
 {
-//    override func constrainBoundsRect(_ proposedBounds: NSRect) -> NSRect {
-//        var rect = super.constrainBoundsRect(proposedBounds)
-//        
-//        if let containerView = documentView {
-//            let containerSize = containerView.intrinsicContentSize // can change on current layout pass and have not yet affect just containerView.frame
-//            
-//            if (rect.size.width > containerSize.width) {
-//                rect.origin.x = (containerSize.width - rect.width) / 2
-//            }
-//            
-//            if(rect.size.height > containerSize.height) {
-//                rect.origin.y = (containerSize.height - rect.height) / 2
-//            }
-//        }
-//        
-//        return rect
-//    }
+    override func constrainBoundsRect(_ proposedBounds: NSRect) -> NSRect {
+        var rect = super.constrainBoundsRect(proposedBounds)
+        
+        if let containerView = documentView {
+            let containerSize = containerView.intrinsicContentSize // can change on current layout pass and have not yet affect just containerView.frame
+            
+            if (rect.size.width > containerSize.width) {
+                rect.origin.x = (containerSize.width - rect.width) / 2
+            }
+            
+            if(rect.size.height > containerSize.height) {
+                rect.origin.y = (containerSize.height - rect.height) / 2
+            }
+        }
+        
+        return rect
+    }
     
+    // TODO: Set from another place
     override var contentInsets: NSEdgeInsets {
         get {
             let inset: CGFloat = 5000
