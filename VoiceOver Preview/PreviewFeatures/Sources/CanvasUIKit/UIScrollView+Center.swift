528cec0afed4fddc8042ab3ca9ff593de8796079
Mikhail Rubanov <akaDuality@yandex.ru>
2024-01-11 21:02:34 -0300

    Improre zooming for iPad

--- VoiceOver Preview/PreviewFeatures/Sources/CanvasUIKit/UIScrollView+Center.swift
+++ VoiceOver Preview/PreviewFeatures/Sources/CanvasUIKit/UIScrollView+Center.swift
@@ -6,8 +6,9 @@ extension UIScrollView {
         self.contentSize = contentSize
         
         /// Fit to width for iPhone, keep image width for iPhone's screen on iPad
-        let minimalWidth = min(bounds.width, contentSize.width)
-        let scale = updateZoomScaleToFitContent(width: minimalWidth)
+//        let minimalWidth = min(bounds.width, contentSize.width)
+        
+        let scale = updateZoomScaleToFitContent(contentSize: contentSize)
         
         // We had to calculate manually because first layout do it wrong
         let scaledContentSize = CGSize(width: contentSize.width * scale,
@@ -22,11 +23,14 @@ extension UIScrollView {
         contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
     }
     
-    func updateZoomScaleToFitContent(width: CGFloat) -> CGFloat {
-        let widthScale  = width  / contentSize.width
-        let minScale    = widthScale
+    func updateZoomScaleToFitContent(contentSize: CGSize) -> CGFloat {
+        let widthScale  = bounds.width  / contentSize.width
+        let heightScale = bounds.height / contentSize.height
         
-        minimumZoomScale = widthScale
+        let isCompactSizeClass = traitCollection.horizontalSizeClass == .compact
+        let minScale = isCompactSizeClass ? widthScale : heightScale
+        
+        minimumZoomScale = minScale
         zoomScale = minScale
         return minScale
     }
